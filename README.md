90947d0aa9bb1db91b7bf8b8a33f748153189363
ShortPupil <Zzh2625@Gmail.com>
2019-04-28 16:53:43 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+总包标注
+
+[![License AGPL-3.0](https://img.shields.io/badge/License-AGPL--3-brightgreen.svg)]()
--- src/main/java/crowdtag/model/businesslogic/aliyunoss/AliyunOSSClientUtil.java
+++ src/main/java/crowdtag/model/businesslogic/aliyunoss/AliyunOSSClientUtil.java
@@ -39,7 +39,7 @@ public class AliyunOSSClientUtil {
 		ACCESS_KEY_SECRET = OSSClientConstants.ACCESS_KEY_SECRET;
 		BACKET_NAME = OSSClientConstants.BACKET_NAME;
 	}
-	
+
 	/**
 	 * 获取阿里云OSS客户端对象
 	 * @return ossClient
@@ -66,17 +66,17 @@ public class AliyunOSSClientUtil {
 		}
 		return bucketNames;
 	}
-	
+
 	/**
 	 * 删除存储空间buckName
 	 * @param ossClient  oss对象
 	 * @param bucketName  存储空间
 	 */
-    public static  void deleteBucket(OSSClient ossClient, String bucketName){  
-    	ossClient.deleteBucket(bucketName);   
-    	System.out.println("删除" + bucketName + "Bucket成功");  
-    }  
-	
+    public static  void deleteBucket(OSSClient ossClient, String bucketName){
+    	ossClient.deleteBucket(bucketName);
+    	System.out.println("删除" + bucketName + "Bucket成功");
+    }
+
 	/**
 	 * 创建模拟文件夹
 	 * @param ossClient oss连接
@@ -85,7 +85,7 @@ public class AliyunOSSClientUtil {
 	 * @return  文件夹名
 	 */
 	public  static String createFolder(OSSClient ossClient,String bucketName,String folder){
-		//文件夹名 
+		//文件夹名
 		final String keySuffixWithSlash =folder;
 		//判断文件夹是否存在，不存在则创建
 		if(!ossClient.doesObjectExist(bucketName, keySuffixWithSlash)){
@@ -99,19 +99,19 @@ public class AliyunOSSClientUtil {
 		}
 		return keySuffixWithSlash;
 	}
-	
-	 /**  
-	    * 根据key删除OSS服务器上的文件  
+
+	 /**
+	    * 根据key删除OSS服务器上的文件
 	    * @param ossClient  oss连接
-	    * @param bucketName  存储空间 
+	    * @param bucketName  存储空间
 	    * @param folder  模拟文件夹名 如"qj_nanjing/"
 	    * @param key Bucket下的文件的路径名+文件名 如："upload/cake.jpg"
-	    */    
-	   public static void deleteFile(OSSClient ossClient, String bucketName, String folder, String key){    
-		    ossClient.deleteObject(bucketName, folder + key);   
-		    System.out.println("删除" + bucketName + "下的文件" + folder + key + "成功");  
-	   } 
-	   
+	    */
+	   public static void deleteFile(OSSClient ossClient, String bucketName, String folder, String key){
+		    ossClient.deleteObject(bucketName, folder + key);
+		    System.out.println("删除" + bucketName + "下的文件" + folder + key + "成功");
+	   }
+
 	   /**
 	     * 对.zip文件进行解压缩
 	     * @param zipFile  解压缩文件
@@ -139,7 +139,7 @@ public class AliyunOSSClientUtil {
 						while( (len = _in.read(_byte)) > 0){
 	                        _out.write(_byte, 0, len);
 	                    }
-	                    _in.close(); 
+	                    _in.close();
 	                    _out.flush();
 	                    _out.close();
 	                    _list.add(_file) ;
@@ -149,7 +149,7 @@ public class AliyunOSSClientUtil {
 	        }
 	        return _list ;
 	    }
-	   
+
 	/**
 	 * 上传图片至OSS
 	 * @param ossClient  oss连接
@@ -159,45 +159,45 @@ public class AliyunOSSClientUtil {
 	 * @return ArrayList<String> 返回的图片网络地址组成的list
 	 * */
 	public static  ArrayList<String> uploadObject2OSS(OSSClient ossClient, ArrayList<File> files, String bucketName, String folder) {
-		ArrayList<String> resultPath =  new ArrayList<String>();  
-        try {  
+		ArrayList<String> resultPath =  new ArrayList<String>();
+        try {
         	for(int i=0 ; i<files.size() ; i++) {
         		File file = files.get(i);
-        		//以输入流的形式上传文件  
-                InputStream is = new FileInputStream(file);  
-                //文件名  
-                String fileName = file.getName();   
-                //文件大小  
-                Long fileSize = file.length();   
-                //创建上传Object的Metadata    
-                ObjectMetadata metadata = new ObjectMetadata();  
-                //上传的文件的长度  
-                metadata.setContentLength(is.available());    
-                //指定该Object被下载时的网页的缓存行为  
-                metadata.setCacheControl("no-cache");   
-                //指定该Object下设置Header  
-                metadata.setHeader("Pragma", "no-cache");    
-                //指定该Object被下载时的内容编码格式  
-                metadata.setContentEncoding("utf-8");    
-                //文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，  
-                //如果没有扩展名则填默认值application/octet-stream  
-                metadata.setContentType(getContentType(fileName));    
-                //指定该Object被下载时的名称（指示MINME用户代理如何显示附加的文件，打开或下载，及文件名称）  
-                metadata.setContentDisposition("filename/filesize=" + fileName + "/" + fileSize + "Byte.");    
-                //上传文件   (上传文件流的形式)  
+        		//以输入流的形式上传文件
+                InputStream is = new FileInputStream(file);
+                //文件名
+                String fileName = file.getName();
+                //文件大小
+                Long fileSize = file.length();
+                //创建上传Object的Metadata
+                ObjectMetadata metadata = new ObjectMetadata();
+                //上传的文件的长度
+                metadata.setContentLength(is.available());
+                //指定该Object被下载时的网页的缓存行为
+                metadata.setCacheControl("no-cache");
+                //指定该Object下设置Header
+                metadata.setHeader("Pragma", "no-cache");
+                //指定该Object被下载时的内容编码格式
+                metadata.setContentEncoding("utf-8");
+                //文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
+                //如果没有扩展名则填默认值application/octet-stream
+                metadata.setContentType(getContentType(fileName));
+                //指定该Object被下载时的名称（指示MINME用户代理如何显示附加的文件，打开或下载，及文件名称）
+                metadata.setContentDisposition("filename/filesize=" + fileName + "/" + fileSize + "Byte.");
+                //上传文件   (上传文件流的形式)
                 @SuppressWarnings("unused")
-				PutObjectResult putResult = ossClient.putObject(bucketName, folder + fileName, is, metadata);    
-                //解析结果  
-                //resultStr = putResult.getETag();  
+				PutObjectResult putResult = ossClient.putObject(bucketName, folder + fileName, is, metadata);
+                //解析结果
+                //resultStr = putResult.getETag();
                 String path = "https://songzi-picture.oss-cn-shenzhen.aliyuncs.com/" + folder + fileName;
                 resultPath.add(path);
         	}
-            
-        } catch (Exception e) {  
-            e.printStackTrace();  
-             System.out.println("上传阿里云OSS服务器异常." + e.getMessage());    
-        }  
-        return resultPath;  
+
+        } catch (Exception e) {
+            e.printStackTrace();
+             System.out.println("上传阿里云OSS服务器异常." + e.getMessage());
+        }
+        return resultPath;
 	}
 
 	/**
@@ -238,9 +238,9 @@ public class AliyunOSSClientUtil {
 		//默认返回类型
 		return "image/jpeg";
 	}
-	
+
 	//方法使用实例
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		//初始化OSSClient
 		OSSClient ossClient=getOSSClient();
 		//要上传的zip文件，本地地址
@@ -248,15 +248,15 @@ public class AliyunOSSClientUtil {
 		//解压
 		ArrayList<File> files = upzipFile(zipFile,"/home/songzi/se_file/1");
 		//上传
-		ArrayList<String> md5key = uploadObject2OSS(ossClient, files, BACKET_NAME, "1/");  
-		System.out.println("上传后的文件MD5数字唯一签名:" + md5key);  
-		
+		ArrayList<String> md5key = uploadObject2OSS(ossClient, files, BACKET_NAME, "1/");
+		System.out.println("上传后的文件MD5数字唯一签名:" + md5key);
+
 		OSSClient ossClient2=getOSSClient();
 		File zipFile2 = new File("/home/songzi/se_file/2.zip");
 		ArrayList<File> files2 = upzipFile(zipFile2,"/home/songzi/se_file/2");
-		ArrayList<String> path = uploadObject2OSS(ossClient2, files2, BACKET_NAME, "2/");  
-		System.out.println(path.get(0));  
-	}*/
-	
-	
+		ArrayList<String> path = uploadObject2OSS(ossClient2, files2, BACKET_NAME, "2/");
+		System.out.println(path.get(0));
+	}
+
+
 }
